64befb89df80b373f5a114a19c6b2048a1212130
Charles <charles.bouveret@outlook.fr>
2022-03-02 16:20:52 +0100

    small fixes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -55,10 +55,4 @@ class MainController extends AbstractController
             
         ]);
     }
-
-    #[Route('/logout', name: 'app_logout',methods: ['GET'])]
-    public function logout()
-    {
-        // controller can be blank: it will never be called!
-    }
 }
